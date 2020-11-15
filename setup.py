262a8f61aafbce8399dd3dad9b711bcc819825d6
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2020-11-15 21:19:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'Topsis_KanavMehta_101816020',         # How you named your package folder (MyLib)
-  packages = ['Topsis_KanavMehta_101816020'],   # Chose the same as "name"
+  name = 'Topsis_IshaanPasricha_101816023',         # How you named your package folder (MyLib)
+  packages = ['Topsis_IshaanPasricha_101816023'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library helps in implementing topsis method',   # Give a short description about your library
-  author = 'Kanav Mehta',                   # Type in your name
-  author_email = 'kmehta_be18@thapar.edu',      # Type in your E-Mail
-  url = 'https://github.com/Kanav-119/Topsis_KanavMehta_101816020',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Kanav-119/Topsis_KanavMehta_101816020/archive/v1.0.tar.gz',    # I explain this later on
+  author = 'Ishaan Pasricha',                   # Type in your name
+  author_email = 'ipasricha_be18@thapar.edu',      # Type in your E-Mail
+  url = 'https://github.com/ishaan019/TOPSIS_Ishaan_Pasricha_101816023',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/ishaan019/TOPSIS_Ishaan_Pasricha_101816023/archive/v1.0.tar.gz',    # I explain this later on
   keywords = ['SIMPLE', 'TOPSIS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'os',
